55497938b33dfe41a78e8ba6fceb1d083f4811fe
yangdongjue5510 <djwhy5510@gmail.com>
2021-11-24 17:30:29 +0900

    fix: getPostJoin query

--- JBlogWeb/src/main/java/com/yang/db/UserDAO.java
+++ JBlogWeb/src/main/java/com/yang/db/UserDAO.java
@@ -21,7 +21,7 @@ public class UserDAO {
     private ResultSet rs;
     private String USER_GET = "select * from blog_user where id = ? and password = ?";
     private String USER_SEARCH_BY_USERNAME = "select * from blog_user where user_name ilike ?";
-
+    private String USER_GET_BY_BLOG_ID = "select * from blog_user where USER_ID = ?";
     public UserVO getUser(UserVO vo) {
         UserVO user = null;
         try {
@@ -29,6 +29,44 @@ public class UserDAO {
             stmt = conn.prepareStatement(USER_GET);
             stmt.setString(1, vo.getId());
             stmt.setString(2, vo.getPassword());
+            rs = stmt.executeQuery();
+
+            while (rs.next()){
+                user = setUserVO();
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        } finally {
+            JDBCUtil.close(rs, stmt, conn);
+        }
+        return user;
+    }
+
+    public List<UserVO> searchUserByUserName(String userName) {
+        List<UserVO> list = new ArrayList<>();
+        try {
+            conn = JDBCUtil.getConnection();
+            stmt = conn.prepareStatement(USER_SEARCH_BY_USERNAME);
+            stmt.setString(1, "%"+userName+"%");
+            rs = stmt.executeQuery();
+            while (rs.next()) {
+                UserVO user = setUserVO();
+                list.add(user);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            JDBCUtil.close(stmt, conn);
+        }
+        return list;
+    }
+
+    public UserVO getUserByBlogId(int blogId) {
+        UserVO user = null;
+        try {
+            conn = JDBCUtil.getConnection();
+            stmt = conn.prepareStatement(USER_GET_BY_BLOG_ID);
+            stmt.setInt(1, blogId);
             rs = stmt.executeQuery();
 
             while (rs.next()){
@@ -52,24 +90,7 @@ public class UserDAO {
         return user;
     }
 
-    public List<UserVO> searchUserByUserName(String userName) {
-        List<UserVO> list = new ArrayList<>();
-        try {
-            conn = JDBCUtil.getConnection();
-            stmt = conn.prepareStatement(USER_SEARCH_BY_USERNAME);
-            stmt.setString(1, "%"+userName+"%");
-            rs = stmt.executeQuery();
-            while (rs.next()) {
-                UserVO user = setUserVO();
-                list.add(user);
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } finally {
-            JDBCUtil.close(stmt, conn);
-        }
-        return list;
-    }
+
 
 //    @Autowired
 //    private JdbcTemplate jdbcTemplate;
